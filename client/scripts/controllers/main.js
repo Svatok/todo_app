bdfcce4c121ab2bd86f15e73649740b486f23b23
Svatok <s_s_s@ua.fm>
2017-06-17 00:17:54 +0300

    first commit

---
+++ client/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todosAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todosAppApp
+ */
+angular.module('todosAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
